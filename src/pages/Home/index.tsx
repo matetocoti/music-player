142e98d2f0b8d011ceae2ebb9f090882bf160cd4
Matheus Costa <92372784+matetocoti@users.noreply.github.com>
2026-01-28 20:10:38 -0300

    feat: introduce default layout, lazy-loaded routes and song navigation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,24 @@
 import { memo } from "react";
 import MyGridContainer from "../../components/MyGridContainer";
 import SongBox from "../../components/SongBox";
-import songsDB from "../../data/songs.mock";
 import { Link } from "react-router-dom";
+import { useSongs } from "../../hooks/useSongs";
 
 const Home = () => {
+  const { songs, loading, error } = useSongs();
+
+  if (loading) {
+    return <div>Loading songs...</div>;
+  }
+  if (error) {
+    return <div>Error loading songs: {error}</div>;
+  }
   return (
     <main>
       <MyGridContainer>
-        {songsDB.map((song) => (
-          <Link key={song.id} to={`/player/${song.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-            <SongBox key={song.id} song={song} />
+        {songs.map((song) => (
+          <Link key={song.id} to={`/player/${song.id}`}>
+            <SongBox song={song} />
           </Link>
         ))}
       </MyGridContainer>
